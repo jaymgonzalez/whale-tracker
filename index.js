398fd7130a83467ff0a098b3984d525181242830
Jay M Gonzalez <jaimungon@gmail.com>
2022-08-25 21:47:35 +0200

    add sound

--- index.js
+++ index.js
@@ -1,1 +1,159 @@
-// Code your whale tracker script here!
+const ethers = require('ethers')
+const player = require('play-sound')((opts = {}))
+
+const rpcURL = 'https://cloudflare-eth.com/'
+const provider = new ethers.providers.JsonRpcProvider(rpcURL)
+
+const CONTRACT_ADDRESS = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
+const CONTRACT_ABI = [
+  {
+    constant: true,
+    inputs: [],
+    name: 'name',
+    outputs: [{ name: '', type: 'string' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    constant: false,
+    inputs: [
+      { name: '_spender', type: 'address' },
+      { name: '_value', type: 'uint256' },
+    ],
+    name: 'approve',
+    outputs: [{ name: '', type: 'bool' }],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'totalSupply',
+    outputs: [{ name: '', type: 'uint256' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    constant: false,
+    inputs: [
+      { name: '_from', type: 'address' },
+      { name: '_to', type: 'address' },
+      { name: '_value', type: 'uint256' },
+    ],
+    name: 'transferFrom',
+    outputs: [{ name: '', type: 'bool' }],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'decimals',
+    outputs: [{ name: '', type: 'uint8' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    constant: true,
+    inputs: [{ name: '_owner', type: 'address' }],
+    name: 'balanceOf',
+    outputs: [{ name: 'balance', type: 'uint256' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'symbol',
+    outputs: [{ name: '', type: 'string' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    constant: false,
+    inputs: [
+      { name: '_to', type: 'address' },
+      { name: '_value', type: 'uint256' },
+    ],
+    name: 'transfer',
+    outputs: [{ name: '', type: 'bool' }],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    constant: true,
+    inputs: [
+      { name: '_owner', type: 'address' },
+      { name: '_spender', type: 'address' },
+    ],
+    name: 'allowance',
+    outputs: [{ name: '', type: 'uint256' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  { payable: true, stateMutability: 'payable', type: 'fallback' },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, name: 'owner', type: 'address' },
+      { indexed: true, name: 'spender', type: 'address' },
+      { indexed: false, name: 'value', type: 'uint256' },
+    ],
+    name: 'Approval',
+    type: 'event',
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, name: 'from', type: 'address' },
+      { indexed: true, name: 'to', type: 'address' },
+      { indexed: false, name: 'value', type: 'uint256' },
+    ],
+    name: 'Transfer',
+    type: 'event',
+  },
+]
+
+const contract = new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, provider)
+
+const TRANSFER_THRESHOLD = 100000000000
+
+const playSound = () => {
+  player.play('ding.mp3', { mplayer: ['-v', 1] }, function (err) {
+    if (err) throw err
+  })
+}
+
+const main = async () => {
+  const name = await contract.name()
+  playSound()
+  console.log(
+    `Whale tracker started!\nListening for large transactions on ${name}`
+  )
+  contract.on('Transfer', (from, to, amount, data) => {
+    if (amount.toNumber() >= TRANSFER_THRESHOLD) {
+      playSound()
+      console.log(
+        `New whale transfer for $${(amount.toNumber() / 1000000)
+          .toFixed(2)
+          .toString()
+          .replace(
+            /\B(?=(\d{3})+(?!\d))/g,
+            ','
+          )} ${name}: https://etherscan.io/tx/${data.transactionHash}`
+      )
+    }
+    // console.log(from, to, amount, data)
+  })
+}
+
+main()
