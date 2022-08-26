4368100c133affef4dbf012ed5741162f70dbe73
Jay M Gonzalez <jaimungon@gmail.com>
2022-08-26 17:54:08 +0200

    add twitter API key

--- index.js
+++ index.js
@@ -127,6 +127,13 @@ const contract = new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, provider)
 
 const TRANSFER_THRESHOLD = 100000000000
 
+const client = new TwitterApi({
+  appKey: process.env.TWITTER_API_KEY,
+  appSecret: process.env.TWITTER_API_KEY_SECRET,
+  accessToken: process.env.TWITTER_ACCESS_TOKEN,
+  accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+})
+
 const main = async () => {
   const name = await contract.name()
   console.log(
