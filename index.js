efb09af0148a15b51017615ffd365cc9d8c3b8c4
Jay M Gonzalez <jaimungon@gmail.com>
2022-09-03 20:24:49 +0200

    add stable info

--- index.js
+++ index.js
@@ -4,127 +4,6 @@ const twitterCLient = require('./twitterClient.js')
 const rpcURL = 'https://cloudflare-eth.com/'
 const provider = new ethers.providers.JsonRpcProvider(rpcURL)
 
-const CONTRACT_ADDRESS = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
-const CONTRACT_ABI = [
-  {
-    constant: true,
-    inputs: [],
-    name: 'name',
-    outputs: [{ name: '', type: 'string' }],
-    payable: false,
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    constant: false,
-    inputs: [
-      { name: '_spender', type: 'address' },
-      { name: '_value', type: 'uint256' },
-    ],
-    name: 'approve',
-    outputs: [{ name: '', type: 'bool' }],
-    payable: false,
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: 'totalSupply',
-    outputs: [{ name: '', type: 'uint256' }],
-    payable: false,
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    constant: false,
-    inputs: [
-      { name: '_from', type: 'address' },
-      { name: '_to', type: 'address' },
-      { name: '_value', type: 'uint256' },
-    ],
-    name: 'transferFrom',
-    outputs: [{ name: '', type: 'bool' }],
-    payable: false,
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: 'decimals',
-    outputs: [{ name: '', type: 'uint8' }],
-    payable: false,
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    constant: true,
-    inputs: [{ name: '_owner', type: 'address' }],
-    name: 'balanceOf',
-    outputs: [{ name: 'balance', type: 'uint256' }],
-    payable: false,
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: 'symbol',
-    outputs: [{ name: '', type: 'string' }],
-    payable: false,
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    constant: false,
-    inputs: [
-      { name: '_to', type: 'address' },
-      { name: '_value', type: 'uint256' },
-    ],
-    name: 'transfer',
-    outputs: [{ name: '', type: 'bool' }],
-    payable: false,
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
-    constant: true,
-    inputs: [
-      { name: '_owner', type: 'address' },
-      { name: '_spender', type: 'address' },
-    ],
-    name: 'allowance',
-    outputs: [{ name: '', type: 'uint256' }],
-    payable: false,
-    stateMutability: 'view',
-    type: 'function',
-  },
-  { payable: true, stateMutability: 'payable', type: 'fallback' },
-  {
-    anonymous: false,
-    inputs: [
-      { indexed: true, name: 'owner', type: 'address' },
-      { indexed: true, name: 'spender', type: 'address' },
-      { indexed: false, name: 'value', type: 'uint256' },
-    ],
-    name: 'Approval',
-    type: 'event',
-  },
-  {
-    anonymous: false,
-    inputs: [
-      { indexed: true, name: 'from', type: 'address' },
-      { indexed: true, name: 'to', type: 'address' },
-      { indexed: false, name: 'value', type: 'uint256' },
-    ],
-    name: 'Transfer',
-    type: 'event',
-  },
-]
-
-const contract = new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, provider)
-
 const TRANSFER_THRESHOLD = 1000000000000
 
 const tweet = async (tweet) => {
@@ -134,6 +13,11 @@ const tweet = async (tweet) => {
   } catch (err) {
     console.error(err)
   }
+}
+
+const contractCreation = (address, abi, provider) => {
+  const contract = new ethers.Contract(address, abi, provider)
+  return contract
 }
 
 const main = async () => {
@@ -146,18 +30,18 @@ const main = async () => {
   const date = new Date().toISOString().replace('T', ' ').substring(0, 16)
 
   contract.on('Transfer', (from, to, amount, data) => {
-    const message = `New whale transfer for $${(amount.toNumber() / 1000000)
-      .toFixed(2)
-      .toString()
-      .replace(
-        /\B(?=(\d{3})+(?!\d))/g,
-        ','
-      )} ${name}: https://etherscan.io/tx/${
-      data.transactionHash
-    } on ${date} UTC`
-    if (amount.toNumber() >= TRANSFER_THRESHOLD) {
-      tweet(message)
-    }
+    // const message = `New whale transfer for $${(amount.toNumber() / 1000000)
+    //   .toFixed(2)
+    //   .toString()
+    //   .replace(
+    //     /\B(?=(\d{3})+(?!\d))/g,
+    //     ','
+    //   )} ${name}: https://etherscan.io/tx/${
+    //   data.transactionHash
+    // } on ${date} UTC`
+    // if (amount.toNumber() >= TRANSFER_THRESHOLD) {
+    //   tweet(message)
+    // }
   })
 }
 
