29f566487f844a7e805f7864d9ba02231b97f9d2
Jay M Gonzalez <jaimungon@gmail.com>
2022-09-11 01:56:36 +0200

    add logging to script

--- whaleTrackerCron.sh
+++ whaleTrackerCron.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 process="whale-tracker/index.js"
 makerun="/home/jay/.nvm/versions/node/v16.17.0/bin/node /home/jay/workspace/whale-tracker/index.js"
+logging="/home/jay/workspace/logs/whale-tracker.log"
 if ps aux | grep -v grep | grep --quiet $process
 then
     printf "Process '%s' is running.\n" "$process"
     exit
 else
     printf "Starting process '%s'.\n" "$process"
-    $makerun &
+    $makerun & >> $logging
 fi
 exit
