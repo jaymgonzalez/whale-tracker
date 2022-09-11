498c6b0cae1e5e65db7b59b8416071cd29f4ad6b
Jay M Gonzalez <jaimungon@gmail.com>
2022-09-11 02:10:59 +0200

    fix cron directory issue in launch

--- whaleTrackerCron.sh
+++ whaleTrackerCron.sh
@@ -2,12 +2,15 @@
 process="whale-tracker/index.js"
 makerun="/home/jay/.nvm/versions/node/v16.17.0/bin/node /home/jay/workspace/whale-tracker/index.js"
 logging="/home/jay/workspace/logs/whale-tracker.log"
+projectHome="/home/jay/workspace/whale-tracker"
+
 if ps aux | grep -v grep | grep --quiet $process
 then
     printf "Process '%s' is running.\n" "$process"
     exit
 else
     printf "Starting process '%s'.\n" "$process"
+    cd $projectHome
     $makerun & >> $logging
 fi
 exit
